8cd560df1eeefa2837b2b20e91508bfc49e9526f
Ilija Vukotic <ivukotic@cern.ch>
2022-08-25 22:56:40 -0500

    e

--- align.sh
+++ align.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+bwa mem -t 16 REFERENT/hs37d5.fa.gz CONSTANTIN/NG1TJP54IK_1.fastq.gz CONSTANTIN/NG1TJP54IK_2.fastq.gz |  bgzip -@ 16 > CONSTANTIN/constantin_v37d5_raw.bam
--- clean.sh
+++ clean.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+samtools view -uh --no-PG CONSTANTIN/constantin_v37d5_raw.bam |   samtools fixmate -m -O bam -@ 16 - CONSTANTIN/constantin_v37d5_fixmate.bam
+
+samtools sort -T CONSTANTIN/ -m 1800M -@ 16 /CONSTANTIN/constantin_v37d5_fixmate.bam |   samtools markdup -s -d 2500 -@ 16 - CONSTANTIN/constantin_v37d5.bam 2> CONSTANTIN/constantin_v37d5_markdup.txt
+
+samtools index CONSTANTIN/constantin_v37d5.bam
